4c55261f9b2624d59013eaae8be2c163cef7eec9
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-11-08 20:07:06 +0100

    User entity for its attributes/fields

---
+++ src/main/java/com/figmonie/data/models/User.java
@@ -0,0 +1,61 @@
+package com.figmonie.data.models;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@Document(collation = "users")
+public class User implements UserDetails {
+    @Id
+    private String id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private String password;
+
+    private Account account;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
