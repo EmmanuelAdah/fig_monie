389f36cad7a197316b6be9f06ba003515ad7be51
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-11-08 20:06:28 +0100

    users repository for manipulating the database

---
+++ src/main/java/com/figmonie/data/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.figmonie.data.repositories;
+
+import com.figmonie.data.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+}
