841dca96e8468f81ce73acdef3919c80d8cff622
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-12-03 21:32:51 +0100

    message controller

--- src/main/java/com/figmonie/controllers/MessageController.java
+++ src/main/java/com/figmonie/controllers/MessageController.java
@@ -1,7 +1,8 @@
 package com.figmonie.controllers;
 
-import com.figmonie.publisher.RabbitMQProducer;
+import com.figmonie.services.RabbitMQProducer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,8 @@ public class MessageController {
     private final RabbitMQProducer rabbitMQProducer;
 
     @PostMapping("/send")
-    public void sendMessage(@RequestBody String message){
+    public ResponseEntity<String> sendMessage(@RequestBody String message){
         rabbitMQProducer.sendMessage(message);
+        return ResponseEntity.ok("Message sent successfully");
     }
 }
