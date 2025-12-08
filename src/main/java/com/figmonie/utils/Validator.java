3c993c3458ef528467d7c553a8f7d76e2f2963cb
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-12-08 19:08:07 +0100

    feat:- validator

--- src/main/java/com/figmonie/utils/Validator.java
+++ src/main/java/com/figmonie/utils/Validator.java
@@ -1,8 +1,12 @@
 package com.figmonie.utils;
 
 import com.figmonie.dtos.request.RegisterRequest;
+import com.figmonie.exceptions.InvalidAmountException;
 import com.figmonie.exceptions.InvalidEmailFormatException;
 import com.figmonie.exceptions.InvalidNameFormatException;
+import com.figmonie.exceptions.InvalidPinFormatException;
+
+import java.math.BigDecimal;
 import java.util.regex.Pattern;
 
 public class Validator {
@@ -22,8 +26,27 @@ public class Validator {
 
     public static void isValidEmail(String email) {
         String regex = "^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9-]{4,10})\\.[a-zA-Z]{2,3}$";
-        boolean pattern = Pattern.matches(regex, email);
-        if(!pattern)
+        if(!Pattern.matches(regex, email))
             throw new InvalidEmailFormatException("Invalid email format for " + email);
+    }
+
+    public static void isValidPin(String pin){
+        String regex = "^[0-9]{4}$";
+        if(pin.length() != 4 || !pin.matches(regex))
+            throw new InvalidPinFormatException("Pin must contain 4 digits only");
+    }
+
+    public static void isValidAccountNumber(String amountNumber){
+        String regex = "^[0-9]{11}$";
+        if(amountNumber.length() != 11 || !amountNumber.matches(regex))
+            throw new InvalidPinFormatException("Account number must contain 11 digits only");
+    }
+
+    public static void isValidAmount(BigDecimal amount, BigDecimal balance){
+        if (amount.compareTo(BigDecimal.ZERO) <= 0)
+            throw new InvalidAmountException("Amount must be greater than zero");
+
+        if (amount.compareTo(balance) > 0)
+            throw new InvalidAmountException("Insufficient funds");
     }
 }
