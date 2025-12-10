2752158906badcf0b6a4bf8490600825d1ffc491
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-12-10 21:13:45 +0100

    feat:- spring security config

--- src/main/java/com/figmonie/security/SecurityConfig.java
+++ src/main/java/com/figmonie/security/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            http
+        http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/api/auth/figmonie/**")
