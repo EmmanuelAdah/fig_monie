9e493045f7d484e75e6165a9ed7724e8cc01eb40
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-12-12 12:58:55 +0100

    feat:- main

--- src/main/java/com/figmonie/FigMonieApplication.java
+++ src/main/java/com/figmonie/FigMonieApplication.java
@@ -2,8 +2,10 @@ package com.figmonie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class FigMonieApplication {
 
     public static void main(String[] args) {
