c0499db6a38faea0d1099a3a70abf728a425af75
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-12-20 17:32:29 +0100

    feat:- account

--- src/main/java/com/figmonie/data/models/Account.java
+++ src/main/java/com/figmonie/data/models/Account.java
@@ -1,13 +1,14 @@
 package com.figmonie.data.models;
 
+import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import java.math.BigDecimal;
 import java.util.List;
 
 @Data
+@Embeddable
 @AllArgsConstructor
 @NoArgsConstructor
 public class Account {
@@ -16,6 +17,5 @@ public class Account {
     private Saving savings;
     private String transactionPin;
 
-    @DBRef
     private List<Transaction> transactions;
 }
